aa9a2ad71f84992034652d594ee8bf0149472ef1
Luzieewert <luzieewert@aol.de>
2020-03-30 21:22:34 +0200

    Add online documentation

---
+++ Screendesign/mi-bachelor-screendesign-doku/README.md
@@ -0,0 +1,3 @@
+# Startercode Online Dokumentation
+
+Your final portfolio might look like [this](http://students.pages.coco.study/ws1920/df12/startercode-gestaltungsportfolio).
---
+++ Screendesign/mi-bachelor-screendesign-doku/js/helper.js
@@ -0,0 +1,9 @@
+/* ----------------------------------------------------------------------------
+helper.js                                                                
+Enthält ein paar kleine Helferlein für die Online Dokumentation in Screendesign. 
+Kurs «Screendesign» an der TH Köln
+Christian Noss
+christian.noss@th-koeln.de
+https://twitter.com/cnoss
+https://th-koeln.github.io/mi-bachelor-screendesign/
+----------------------------------------------------------------------------*/
