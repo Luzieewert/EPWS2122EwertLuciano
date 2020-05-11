d9408e678366fc654679a3f858f6f7ff2b2da3da
Büsra Sancak (inf1343) <buesra.sancak@smail.th-koeln.de>
2020-05-11 21:18:06 +0200

    Client und Server

---
+++ Praktikum/cmake-build-debug/CMakeFiles/Praktikum.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/BS/CLionProjects/Praktikum/Client.c" "/mnt/c/Users/BS/CLionProjects/Praktikum/cmake-build-debug/CMakeFiles/Praktikum.dir/Client.c.o"
+  "/mnt/c/Users/BS/CLionProjects/Praktikum/Server.c" "/mnt/c/Users/BS/CLionProjects/Praktikum/cmake-build-debug/CMakeFiles/Praktikum.dir/Server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
