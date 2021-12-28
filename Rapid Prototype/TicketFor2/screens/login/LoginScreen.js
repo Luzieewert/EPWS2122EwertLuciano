566f0417ff1031db428c22bfdd1edd2c19d414e7
Steytz <vluciano@codevise.de>
2021-12-28 13:54:33 +0100

    Refactor Login Screen
    - Extracted Styles
    - Re-used new Post function

--- Rapid Prototype/TicketFor2/screens/login/LoginScreen.js
+++ Rapid Prototype/TicketFor2/screens/login/LoginScreen.js
@@ -1,54 +1,33 @@
 import React, {useContext, useState} from 'react';
-import { View, TextInput } from 'react-native';
+import {View, TextInput} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import GenericButton from "../components/GenericButton";
-import axios from "axios";
 import {UserContext} from "../../contexts/UserContext";
+import styles from "./styles";
+import text from "../../theme/text";
+import {handlePost} from "../../utils/databaseInteraction";
 
+const loginUrl = "http://localhost:8001/TicketFor2/login"
 
-const BUTTON = {
-    backgroundColor: 'black',
-    elevation: 5,
-    padding: 5,
-    alignItems: "center",
-    marginHorizontal: 8,
-    marginBottom: 8
-}
-const TEXTINPUT = {borderRadius: 5, borderWidth: 1, borderColor: "black", marginBottom: 7}
-
-
-const handleRegistrationPress = (navigation) => {
-    navigation.navigate('Registration')
-    return null;
-};
 const LoginScreen = () => {
     const navigation = useNavigation();
     const [loginData, setLoginData] = useState({})
     const [user, setUser] = useContext(UserContext)
-    const handleLogin = async (data) => {
-        await axios.post('http://localhost:8001/TicketFor2/login', data)
-            .then((res) => {
-                setUser(res.data.user)
-                navigation.reset({index: 0, routes: [{name: 'Map'}]})
-                navigation.navigate('Map')
-            })
-            .catch((err) => {
-                console.log(err);
-            });
 
-    };
+    const handleLoginSuccess = (res) => {
+        setUser(res.data.user)
+        navigation.reset({index: 0, routes: [{name: 'Map'}]})
+    }
 
     return (
-        <View style={{paddingHorizontal: 8}}>
-
-            <TextInput style={{...TEXTINPUT, marginTop: 8}}
+        <View style={styles.container}>
+            <TextInput style={styles.textInput}
                        placeholder="Email"
                        placeholderTextColor="#003f5c"
                        onChangeText={(e) => setLoginData({...loginData, email: e})}
             />
 
-
-            <TextInput style={TEXTINPUT}
+            <TextInput style={styles.textInput}
                        placeholder="Password"
                        placeholderTextColor="#003f5c"
                        secureTextEntry={true}
@@ -56,15 +35,12 @@ const LoginScreen = () => {
             />
 
 
-            <GenericButton buttonText="Anmelden" buttonStyle={BUTTON} textStyle={{color: 'white'}}
-                           onPress={() => handleLogin(loginData)}/>
+            <GenericButton buttonText="Anmelden" buttonStyle={styles.button} textStyle={text.inButton}
+                           onPress={() => handlePost(loginUrl, loginData, handleLoginSuccess)}/>
 
-            <GenericButton buttonText="Registrieren" buttonStyle={BUTTON} textStyle={{color: 'white'}}
-                           onPress={() => handleRegistrationPress(navigation)}/>
-
+            <GenericButton buttonText="Registrieren" buttonStyle={styles.button} textStyle={text.inButton}
+                           onPress={() => navigation.navigate("Registration")}/>
         </View>
-
-
     );
 };
 
