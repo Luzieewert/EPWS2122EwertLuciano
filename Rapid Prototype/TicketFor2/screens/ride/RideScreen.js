81c3910e0ab400d89eb3f4e0de5d6e62ca2903ad
Luzie Undine Charlotta Zoe Ewert (inf1341) <luzie_undine_charlotta_zoe.ewert@smail.th-koeln.de>
2022-01-25 20:07:30 +0100

    add Ride Progress

--- Rapid Prototype/TicketFor2/screens/ride/RideScreen.js
+++ Rapid Prototype/TicketFor2/screens/ride/RideScreen.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react"
+import React, {useContext, useEffect, useState} from "react"
 import {View} from "react-native";
 import GenericButton from "../components/GenericButton";
 import {useNavigation} from "@react-navigation/native";
@@ -9,15 +9,21 @@ import text from "../../theme/text";
 import {handleGet, handlePut} from "../../utils/databaseInteraction";
 import {urls} from "../../utils/urls";
 import {createChat} from "./utils";
+import MapView from "react-native-maps";
+import {onUserLocationChange} from "../../utils/location";
+import {findOtherUser, renderCurrentRideMarker, renderMarkers} from "../Map/utils";
+import Map from "../Map/Map";
 
 const RideScreen = ({route}) => {
     const [ride, setRide] = useContext(RideContext)
     const [user, setUser] = useContext(UserContext)
     const {mode} = route.params
     const navigation = useNavigation()
+    const [renderObjects, setRenderObjects] = useState([])
+    const [location, setLocation] = useState({})
 
     const updateRideCallback = (res) => {
-        const newRide = res.data.ride
+        const newRide = res
         setRide(newRide)
 
         if (newRide.ride_status === "Completed") {
@@ -30,30 +36,43 @@ const RideScreen = ({route}) => {
         if (mode === "Creator") {
             createChat(ride._id, null)
         }
+
     }, [])
 
 
     useEffect(() => {
-        const handle = setInterval(() => handleGet(urls.ride + ride._id, updateRideCallback), 5000)
+        const handle = setInterval(() => handleGet(urls.ride + ride._id, null, updateRideCallback), 5000)
         return () => {
             clearInterval(handle)
         }
     }, [ride])
 
+    const endRideCallback = (res) => {
+        navigation.reset({
+            index: 0,
+            routes: [{name: 'PostRide'}]
+        })}
+
+    const rideStart = ride.ride_status === "Started" && mode === "Creator"
 
     return (
-        <View style={styles.container}>
+        <>
+            <Map ride={ride} renderObjects={renderObjects} onUserLocationCallback={setLocation} location={location} otherUser={findOtherUser(ride)} setRide={setRide} />
+            <View style = {styles.container}>
+
+                { rideStart && <GenericButton onPress={() => handlePut(urls.ride + ride._id,{ride_status: "Progress"}, updateRideCallback )} buttonStyle={styles.button}
+                               textStyle={text.inButton}
+                               buttonText="Fahrt beginnen!"/>}
+
             <GenericButton onPress={() => navigation.navigate("Chat")} buttonStyle={styles.button}
                            textStyle={text.inButton}
                            buttonText="Chat"/>
-            {mode === "Creator" &&
-                <GenericButton onPress={() => handlePut(urls.ride + ride._id, () => navigation.reset({
-                    index: 0,
-                    routes: [{name: 'PostRide'}]
-                }))} buttonStyle={styles.button}
+
+                <GenericButton onPress={() => handlePut(urls.ride + ride._id,{ride_status: "Completed"}, endRideCallback )} buttonStyle={styles.button}
                                textStyle={text.inButton}
-                               buttonText="End Ride"/>}
-        </View>
+                               buttonText="Fahrt beenden"/>
+            </View>
+        </>
     )
 }
 
